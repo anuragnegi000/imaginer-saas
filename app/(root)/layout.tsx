53bec8e37ae29c689570ab83b66f6c478d1f16bf
anuragnegi000 <anurag.negi862@gmail.com>
2024-02-18 19:07:29 +0530

    created a high-level side-bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="root">
         <Sidebar/>
       <div className="root-container">
-        <div className="wrapper">{children}</div>
+        {/* <div className="wrapper">{children}</div> */}
       </div>
     </main>
   );
