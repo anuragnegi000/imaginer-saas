13d6c47a1350197528722d604a3156e35c487fad
anuragnegi000 <anurag.negi862@gmail.com>
2024-02-19 20:28:40 +0530

    turned mobile nav into basic

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import {
   Sheet,
   SheetContent,
@@ -9,12 +9,13 @@ import {
 } from "@/components/ui/sheet";
 import Link from "next/link";
 import Image from "next/image";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { navLinks } from "@/constants";
 import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
 
 const MobileNav = () => {
-  const pathname=usePathname();
+  const pathname = usePathname();
   return (
     <header className="header">
       <Link href="/" className="flex items-center gap-2 md:py-2">
@@ -30,78 +31,56 @@ const MobileNav = () => {
           <UserButton afterSignOutUrl="/"></UserButton>
           <Sheet>
             <SheetTrigger>
-              <Image src="/assets/icons/menu.svg"
-              alt="menu"
-              width={32}
-              height={32}
+              <Image
+                src="/assets/icons/menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
               />
             </SheetTrigger>
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Are you absolutely sure?</SheetTitle>
-                <SheetDescription>
-                <ul className="sidebar-nav-elements">
-              {navLinks.slice(0, 6).map((link) => {
-                const isActive = link.route === pathname;
+            <SheetContent className="sheet-content sm:w-64">
+              <>
+                <Image
+                  src="/assets/images/logo-text.svg"
+                  alt="logo"
+                  width={152}
+                  height={23}
+                  className="p-4"
+                />
+                <ul className="header-nav-elements">
+                  {navLinks.map((link) => {
+                    const isActive = link.route === pathname;
 
-                return (
-                  <li
-                    key={link.route}
-                    className={`sidebar-nav_element group ${
-                      isActive
-                        ? "bg-purple-gradient text-white"
-                        : "text-gray-700"
-                    }`}
-                  >
-                    <Link className="sidebar-link" href={link.route}>
-                      <Image
-                        src={link.icon}
-                        alt="logo"
-                        width={24}
-                        height={24}
-                        className={`${isActive && "brightness-200"}`}
-                      />
-                      {link.label}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-            <ul className="sidebar-nav-elements">
-              {navLinks.slice(6).map((link) => {
-                const isActive = link.route === pathname;
-
-                return (
-                  <li
-                    key={link.route}
-                    className={`sidebar-nav_element group ${
-                      isActive
-                        ? "bg-purple-gradient text-white"
-                        : "text-gray-700"
-                    }`}
-                  >
-                    <Link className="sidebar-link" href={link.route}>
-                      <Image
-                        src={link.icon}
-                        alt="logo"
-                        width={24}
-                        height={24}
-                        className={`${isActive && "brightness-200"}`}
-                      />
-                      {link.label}
-                    </Link>
-                  </li>
-                );
-              })}
-              <li className="flex-center cursor-pointer gap-2 p-4">
-                <UserButton afterSignOutUrl="/" showName />
-              </li>
-            </ul>
-                </SheetDescription>
-              </SheetHeader>
+                    return (
+                      <li
+                        className={`${isActive && "gradient-text"
+                        }p-18 flex whitespace-nowrap text-dark-700`}
+                        key={link.route}
+                      >
+                        <Link className="sidebar-link cursor-pointer" href={link.route}>
+                          <Image
+                            src={link.icon}
+                            alt="logo"
+                            width={24}
+                            height={24}
+                            
+                          />
+                          {link.label}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
             </SheetContent>
           </Sheet>
         </SignedIn>
+
+        <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
       </nav>
     </header>
   );
