235dc9790e2bb94c3a2f6ed3204d7c789813dba3
anuragnegi000 <anurag.negi862@gmail.com>
2024-02-19 23:03:53 +0530

    created mongoDB schema for Imaginer

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,46 @@
+import { transformationTypes } from "@/constants";
+// import { User } from "@clerk/nextjs/server";
+import { Document,Schema, model, models } from "mongoose";
+import { Nanum_Pen_Script } from "next/font/google";
+
+
+const UserSchema=new Schema({
+    clerkId:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    photo:{
+        type:String,
+        required:true,
+    },
+    firstName:{
+        type:String,
+        required:true,
+    },lastName:{
+        type:String,
+        required:true
+    },
+    planId:{
+        type:Number,
+        default:1,
+    },
+     creditBalance:{
+        type:Number,
+        default:10,
+    },
+})
+
+const User=models?.User || model("User",UserSchema);
+
+export default User;
