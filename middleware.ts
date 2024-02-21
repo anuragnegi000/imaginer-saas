0fbc2b00442594ba7a36d10cb780881eb2ad2188
anuragnegi000 <anurag.negi862@gmail.com>
2024-02-22 04:26:45 +0530

    clearing up silly changes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/api/webhooks/clerk '],
+  publicRoutes: ['/api/webhooks/clerk'],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ['/no-auth-in-this-route'],
