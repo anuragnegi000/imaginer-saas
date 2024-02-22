11309b550c3597ad43b95da4d8e4b8fcaaa059c7
anuragnegi000 <anurag.negi862@gmail.com>
2024-02-22 05:32:10 +0530

    added dynamic content

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,11 @@
+import Header from '@/components/shared/Header'
 import React from 'react'
+import { transformationTypes } from '@/constants'
 
-const AddTransformationType = () => {
+const AddTransformationType = ({params:{type}}:SearchParamProps) => {
+  const transformation=transformationTypes[type];
   return (
-    <div>AddTransformationType</div>
+    <Header title={transformation.title} subtitle={transformation.subTitle}/>
   )
 }
 
