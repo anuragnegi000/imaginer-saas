51c7fb65dd6625937613f749726856b32f85e84c
anuragnegi000 <anurag.negi862@gmail.com>
2024-02-22 21:23:18 +0530

    added dynamic forms

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,15 +2,27 @@ import Header from '@/components/shared/Header'
 import React from 'react'
 import { transformationTypes } from '@/constants'
 import TransformationForm from '@/components/shared/TransformationForm';
+import { auth } from '@clerk/nextjs';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-const AddTransformationType = ({params:{type}}:SearchParamProps) => {
+
+const AddTransformationType =async ({params:{type}}:SearchParamProps) => {
   const transformation=transformationTypes[type];
+  const {userId}=auth();
+  if(!userId) redirect('/sign-in')
+  const user=await getUserById(userId);
   return (
     <>    
     <Header title={transformation.title}
      subtitle={transformation.subTitle}
      />
-     <TransformationForm/>
+     <TransformationForm
+     action="Add"
+     userId={user._id}
+     type={transformation.type as TransformationTypeKey}
+     creditBalance={user.creditBalance }
+     />
     </>
 
   )
